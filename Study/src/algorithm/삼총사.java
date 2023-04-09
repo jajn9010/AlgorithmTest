ec45772df219e0928128ae5449a4607e3059704b
Jaejin Lee <jaejin1112@gmail.com>
2023-04-09 22:22:30 +0900

    프로그래머스 삼총사 algorithm

---
+++ Study/src/algorithm/삼총사.java
@@ -0,0 +1,21 @@
+package algorithm;
+
+public class 삼총사 {
+	public static void main(String[] args) {
+		int[] number = {-2, 3, 0, 2, -5};
+		
+		int answer = 0;
+        for(int i = 0; i < number.length-2; i++) {
+            for (int j = i+1; j < number.length-1; j++) {
+                for (int x = j+1; x < number.length; x++) {
+                    if(number[i]+number[j]+number[x] == 0) {
+                        answer += 1;
+                    }
+                }
+            }
+        }
+        
+        System.out.println(answer);
+		
+	}
+}
