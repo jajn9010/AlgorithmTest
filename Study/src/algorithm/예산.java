7b081d1c4585f5a22d5c67b134e67a9eda8fd817
JaejinLee <jaejin1112@gmail.com>
2023-04-15 18:20:58 +0900

    solve algorithm 문자내림차순으로배열하기기 from programmers

--- Study/src/algorithm/예산.java
+++ Study/src/algorithm/예산.java
@@ -4,17 +4,17 @@ import java.util.Arrays;
 
 public class 예산 {
     public static void main(String[] args) {
-        int[] d = {1,3,2,5,4};
+        int[] d = { 1, 3, 2, 5, 4 };
         int budget = 10, answer = 0;
 
         Arrays.sort(d);
 
         for (int i = 0; i < d.length; i++) {
-            if( budget - d[i] >= 0) {
-                answer ++;
+            if (budget - d[i] >= 0) {
+                answer++;
                 budget -= d[i];
-            }
-            else break;
+            } else
+                break;
         }
         System.out.println(answer);
     }
