24a0d6d37038d5d55319e742710b49dd7c937b8a
Jaejin Lee <jaejin1112@gmail.com>
2023-04-18 22:05:50 +0900

    solve algorithm in Backjun 네번째 점

--- Study/src/네번째점/Main.java
+++ Study/src/네번째점/Main.java
@@ -10,17 +10,24 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
-		int [][] x = new int[2][3];
+		int [] x = new int[3];
+		int [] y = new int[3];
 		
 		for (int i = 0; i < 3; i++) {
 			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			
-			x[0][i] = a;
-			x[1][i] = b;
+			x[i] = Integer.parseInt(st.nextToken());
+			y[i] = Integer.parseInt(st.nextToken());
 		}
+		br.close();
 		
-		
+		sb.append(findAxis(x) + " " + findAxis(y));
+		System.out.println(sb);
+	}
+
+	private static String findAxis(int[] x) {
+		if(x[0] == x[1]) return Integer.toString(x[2]);
+		else {
+			return (x[0] == x[2]) ? Integer.toString(x[1]) : Integer.toString(x[0]);
+		}
 	}
 }
