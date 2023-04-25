5312bd214546e2008255d3cef63c1ef45f334935
Jaejin Lee <jaejin1112@gmail.com>
2023-04-25 22:05:54 +0900

    solve one more easy algorithm

--- Study/src/algorithm/D_1697.java
+++ Study/src/algorithm/D_1697.java
@@ -9,44 +9,45 @@ import java.util.StringTokenizer;
 
 public class D_1697 {
 	static int n, k;
-	static int[] visited = new int [100001];
+	static int[] visited = new int[100001];
 	static Queue<Integer> q;
+
 	static int bfs(int x) {
 		q = new LinkedList<>();
 		int index = n;
 		int num = 0;
-		
+
 		visited[index] = 1;
 		q.add(x);
-		while(!q.isEmpty()) {
+		while (!q.isEmpty()) {
 			num = q.remove();
 			if (num == k) {
-				return visited[num] -1;
+				return visited[num] - 1;
 			}
-			if(num-1 >= 0 && visited[num-1] == 0) {
-				visited[num-1] = visited[num]+1;
-				q.add(num-1);
+			if (num - 1 >= 0 && visited[num - 1] == 0) {
+				visited[num - 1] = visited[num] + 1;
+				q.add(num - 1);
 			}
-			if(num+1 <= 100000 && visited[num+1] == 0) {
-				visited[num+1] = visited[num]+1;
-				q.add(num+1);
+			if (num + 1 <= 100000 && visited[num + 1] == 0) {
+				visited[num + 1] = visited[num] + 1;
+				q.add(num + 1);
 			}
-			if(2*num <= 100000 && visited[2*num] == 0) {
-				visited[2*num] = visited[num]+1;
-				q.add(2*num);
+			if (2 * num <= 100000 && visited[2 * num] == 0) {
+				visited[2 * num] = visited[num] + 1;
+				q.add(2 * num);
 			}
 		}
-		
+
 		return -1;
 	}
-	
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
+
 		n = Integer.parseInt(st.nextToken());
 		k = Integer.parseInt(st.nextToken());
-		
+
 		int result = bfs(n);
 		System.out.println(result);
 	}
