f416ce55f5f762e331059ccec5a6e265e62a35ec
Jaejin Lee <jaejin1112@gmail.com>
2023-05-02 22:00:41 +0900

    change algorithm

--- Study/src/algorithm/D1107.java
+++ Study/src/algorithm/D1107.java
@@ -1,47 +1,51 @@
 package algorithm;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.IOException;
 
 public class D1107 {
-	public static void main(String[] args) throws NumberFormatException, IOException {
+	static String N;
+	static int M;
+	static boolean[] arr;
+
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		boolean[] broken = new boolean[10];
-
-		int chan = Integer.parseInt(br.readLine());
-
-		int bNum = Integer.parseInt(br.readLine());
-		st = new StringTokenizer(br.readLine());
-
-		if (bNum != 0) {
-			for (int i = 0; i < bNum; i++) {
-				broken[Integer.parseInt(st.nextToken())] = true;
+		N = br.readLine();
+		M = Integer.parseInt(br.readLine());
+		arr = new boolean[11];
+		if (M > 0) {
+			String[] inputs = br.readLine().split(" ");
+			for (int i = 0; i < M; i++) {
+				arr[Integer.parseInt(inputs[i])] = true;
 			}
 		}
-		System.out.println(calc(chan, broken));
+
+		int ans = Math.abs(Integer.parseInt(N) - 100);
+		for (int i = 0; i < 1000001; i++) {
+			int cnt = check(i);
+			if (cnt != 0)
+				ans = Math.min(ans, Math.abs(Integer.parseInt(N) - i) + check(i));
+		}
+		System.out.print(ans);
 	}
 
-	static int calc(int chan, boolean[] broken) {
-		int result = Math.abs(100 - chan);
-
-		for (int i = 0; i <= 999999; i++) {
-			String curr = Integer.toString(i);
-			boolean check = false;
-
-			for (int j = 0; j < curr.length(); j++) {
-				if (broken[curr.charAt(j) - '0']) {
-					check = true;
-					break;
-				}
-			}
-
-			if (!check) {
-				result = Math.min(result, curr.length() + Math.abs(i - chan));
-			}
+	public static int check(int n) {
+		if (n == 0) {
+			if (arr[n])
+				return 0;
+			else
+				return 1;
 		}
-		return result;
+
+		int cnt = 0;
+		while (n > 0) {
+			if (arr[n % 10])
+				return 0;
+			cnt++;
+			n /= 10;
+		}
+
+		return cnt;
 	}
 }
